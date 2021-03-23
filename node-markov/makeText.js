f4697118687bc8c5339612bc91b09ff232eaeda0
m-zz <marcusyzhu@gmail.com>
2021-03-23 16:28:04 -0700

    step 2 complete

--- node-markov/makeText.js
+++ node-markov/makeText.js
@@ -1,2 +1,28 @@
-/** Command-line tool to generate Markov text. */
+const { MarkovMachine } = require("./markov");
+const fsP = require("fs/promises");
+const axios = require("axios")
+const argv = process.argv;
+
+argv[2] === "file" ? markovFile(argv[3]) : markovURL(argv[3]);
+
+async function markovFile(path) {
+  try {
+    let text = await fsP.readFile(path, "utf8");
+    let mm = new MarkovMachine(text);
+    console.log(mm.getText());
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+async function markovURL(path) {
+  try {
+    let text = await axios.get(path)
+    let mm = new MarkovMachine(text.data);
+    console.log(mm.getText());
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
 
